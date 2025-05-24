5f9dfeda2e9027106a8051f698fe301600884d3b
youngfessy <jarredgeller@comcast.net>
2025-05-23 21:43:09 -0400

    changes to make website

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Important for static export
+  basePath: '/brady-celerbation-app', // Replace with your actual GitHub repo name
+  trailingSlash: true, // GitHub Pages requires trailing slashes
 };
 
 export default nextConfig;
